09ddfe0cf68ec3da2315012d6674e05638bef7a1
Rutuja choudhar <rutuja_choudhar_comp@moderncoe.edu.in>
2025-04-17 18:17:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   res.send('Student CRUD API is running.');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5432;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
